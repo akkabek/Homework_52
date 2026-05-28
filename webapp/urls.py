2d2c424ae36d558d1759c69ccc87ec6ab773152a
Bektur_Akkaziev <bektur@MacBook-Pro-Bektur.local>
2026-05-28 18:47:49 +0600

    Add task detail page, update URLs with names and redirect

--- webapp/urls.py
+++ webapp/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.task_list, name='task_list'),
     path('add/', views.task_add, name='task_add'),
     path('delete/<int:pk>/', views.task_delete, name='task_delete'),
+    path('task/<int:pk>/', views.task_detail, name='task_detail'),
 ]
