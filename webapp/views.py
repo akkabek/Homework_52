8261c7b8719e7c170b6fd5129ac23040f1f07374
Bektur_Akkaziev <bektur@MacBook-Pro-Bektur.local>
2026-05-28 18:47:49 +0600

    Add task detail page, update URLs with names and redirect

--- webapp/views.py
+++ webapp/views.py
@@ -26,3 +26,7 @@ def task_delete(request, pk):
     task = get_object_or_404(Task, pk=pk)
     task.delete()
     return redirect('task_list')
+
+def task_detail(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    return render(request, 'webapp/task_detail.html', {'task': task})
