139a79543b58182503417911ba10581c834858a3
Bektur_Akkaziev <bektur@MacBook-Pro-Bektur.local>
2026-06-04 18:33:31 +0600

    Task -list -add -delete updated. Task -update added. Checkbox delete added and delete confirmation added

--- webapp/urls.py
+++ webapp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('add/', views.task_add, name='task_add'),
     path('delete/<int:pk>/', views.task_delete, name='task_delete'),
     path('task/<int:pk>/', views.task_detail, name='task_detail'),
+    path('update/<int:pk>/', views.task_update, name='task_update'),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -4,29 +4,55 @@ from .models import Task, STATUS_CHOICES
 
 def task_list(request):
     tasks = Task.objects.all()
+    if request.method == 'POST':
+        ids = request.POST.getlist('task_ids')
+        if ids:
+            Task.objects.filter(pk__in=ids).delete()
+        return redirect('task_list')
     return render(request, 'webapp/task_list.html', {'tasks': tasks})
 
 
 def task_add(request):
     if request.method == 'POST':
         description = request.POST.get('description', '').strip()
+        details = request.POST.get('details', '').strip()
         status = request.POST.get('status', 'new')
         due_date = request.POST.get('due_date') or None
         if description:
             Task.objects.create(
                 description=description,
+                details=details,
                 status=status,
                 due_date=due_date
             )
         return redirect('task_list')
-    return render(request, 'webapp/task_add.html', {'status_choices': STATUS_CHOICES})
-
+    return render(request, 'webapp/task_add.html',
+                  {'status_choices': STATUS_CHOICES,
+                  'task' : Task()
+                   })
 
 def task_delete(request, pk):
     task = get_object_or_404(Task, pk=pk)
-    task.delete()
-    return redirect('task_list')
+    if request.method == 'POST':
+        task.delete()
+        return redirect('task_list')
+    return render(request, 'webapp/task_confirm_delete.html', {'task': task})
 
 def task_detail(request, pk):
     task = get_object_or_404(Task, pk=pk)
     return render(request, 'webapp/task_detail.html', {'task': task})
+
+def task_update(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == 'GET':
+        return render(request, 'webapp/task_update.html', {
+            'task': task,
+            'status_choices': STATUS_CHOICES,
+        })
+    elif request.method == 'POST':
+        task.description = request.POST.get('description', '').strip()
+        task.details = request.POST.get('details', '').strip()
+        task.status = request.POST.get('status', 'new')
+        task.due_date = request.POST.get('due_date') or None
+        task.save()
+        return redirect('task_detail', pk)
